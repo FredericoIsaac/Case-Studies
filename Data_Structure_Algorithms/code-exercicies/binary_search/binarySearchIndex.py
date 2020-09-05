ffc193336d922730df0ccc7249fd3fef5d0ecc4e
Frederico Gago <fredyisaac@gmail.com>
2020-09-05 09:13:42 +0100

    binary search

--- Data_Structure_Algorithms/code-exercicies/binary_search/binarySearchIndex.py
+++ Data_Structure_Algorithms/code-exercicies/binary_search/binarySearchIndex.py
@@ -55,6 +55,31 @@ def find_end_index(arr, number, start_index, end_index):
         else:
             start_index = mid_element + 1
     
+
+def recursive_binary_search(target, source, left=0):
+    if len(source) == 0:
+        return None
+    center = (len(source)-1) // 2
+    if source[center] == target:
+        return center + left
+    elif source[center] < target:
+        return recursive_binary_search(target, source[center+1:], left+center+1)
+    else:
+        return recursive_binary_search(target, source[:center], left)
+        
+def find_first(target, source):
+    index = recursive_binary_search(target, source)
+    if not index:
+        return None
+    while source[index] == target:
+        if index == 0:
+            return 0
+        if source[index-1] == target:
+            index -= 1
+        else:
+            return index
+
+
     # Not found
     print(index + "here")
     return -1
